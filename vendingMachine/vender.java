95dd9d589804becbf7edb996132514e6c96e2941
snehil <108514128+snehilgamit@users.noreply.github.com>
2023-07-18 22:58:50 +0530

    Init 💫

--- vendingMachine/vender.java
+++ vendingMachine/vender.java
@@ -1,3 +1,6 @@
+
+// @snehilgamit 
+// This code only for learing purpose.There are many bugs or you can write it in easly, better syntax. i am learing nothing else..... :) 
 import java.util.Scanner;
 
 public class vender {
